739414d1f9283d58a773dd522f050e7d885cb139
medz1511 <134383184+medz1511@users.noreply.github.com>
2023-11-26 13:45:55 +0000

    ajout des autres pieces

--- pieces.js
+++ pieces.js
@@ -1,41 +1,42 @@
 const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json();
 // creation de la variable qui va representer l'article
-const article = pieces[0]
 // creation des element de  l'article 
-const imgElement = document.createElement("img")
-imgElement.src = article.image
+for (let i = 0; i < pieces.length; i++) {
+    const article = pieces[i]
+    const imgElement = document.createElement("img")
+    imgElement.src = article.image
 
-const nomElement = document.createElement("div")
-nomElement.innerText = article.nom
+    const nomElement = document.createElement("div")
+    nomElement.innerText = article.nom
 
-const prixElement = document.createElement("div")
-prixElement.innerText = `Prix ${article.prix}  ${article.prix > 35 ? "$" : "$$"}`
+    const prixElement = document.createElement("div")
+    prixElement.innerText = `Prix ${article.prix}  ${article.prix > 35 ? "$" : "$$"}`
 
-const descriptionElement = document.createElement("div")
-descriptionElement.innerText = article.description ?? "Pas de decription pour le moment."
+    const descriptionElement = document.createElement("div")
+    descriptionElement.innerText = article.description ?? "Pas de decription pour le moment."
 
 
-function statut() {
-    let statut
-    if (article.disponibilite == true) {
-        statut = " En Stock "
-    } else statut = "En rupture de stock "
+    function statut() {
+        let statut
+        if (article.disponibilite == true) {
+            statut = " En Stock "
+        } else statut = "En rupture de stock "
 
-    return statut
+        return statut
+
+    }
+    const disponibiliteElement = document.createElement("p")
+    disponibiliteElement.innerText = statut()
+    const paragraphe = document.createElement("p")
+    const espaceAffichage = document.querySelector('section.fiches')
+
+    espaceAffichage.appendChild(imgElement)
+    espaceAffichage.appendChild(nomElement)
+    espaceAffichage.appendChild(prixElement)
+    espaceAffichage.appendChild(descriptionElement)
+    espaceAffichage.appendChild(disponibiliteElement)
 
 }
-const disponibiliteElement = document.createElement("p")
-disponibiliteElement.innerText = statut()
-const paragraphe = document.createElement("p")
-const espaceAffichage = document.querySelector('section.fiches')
-
-espaceAffichage.appendChild(imgElement)
-espaceAffichage.appendChild(nomElement)
-espaceAffichage.appendChild(prixElement)
-espaceAffichage.appendChild(descriptionElement)
-espaceAffichage.appendChild(disponibiliteElement)
-
-
 
 // imgElement = article.nom
